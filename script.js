b68221751ce2d91d6d896662131dc235ba64d163
Lucy <49279240+LilBean64-hue@users.noreply.github.com>
2026-06-23 19:38:57 -0400

    Added Test Script

---
+++ script.js
@@ -0,0 +1,15 @@
+// Target the title and button elements from the HTML
+const siteTitle = document.getElementById('title');
+const actionButton = document.getElementById('magicButton');
+
+// Add an event listener to run code whenever the button is clicked
+actionButton.addEventListener('click', () => {
+    // Change the text content of the header
+    siteTitle.textContent = "The Magic Worked!";
+    
+    // Change the color of the text dynamically
+    siteTitle.style.color = "#28a745";
+    
+    // Alert the user
+    alert("You successfully manipulated the page with JavaScript!");
+});
