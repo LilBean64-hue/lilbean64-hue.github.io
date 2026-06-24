a6228db2a3efd339c226c4803e71def13d52c1c7
LilBean64 <88251746+LilBean64@users.noreply.github.com>
2026-06-24 11:20:02 -0400

    Testing connecting with a button

--- .github/workflows/client.js
+++ .github/workflows/client.js
@@ -10,7 +10,9 @@ client.messages.on("message", (content) => {
 
 // Login to the server. Replace `archipelago.gg:XXXXX` and `Phar` with the address/url and slot name for your room.
 // If no game is provided, client will connect in "TextOnly" mode, which is fine for this example.
-client.login("archipelago.gg:53591", "Lucy")
-    .then(() => console.log("Connected to the Archipelago server!"))
-    .catch(console.error);
-client.login
+export async function connectArchi(port, portName){
+    client.login(port, portName)
+        .then(() => console.log("Connected to the Archipelago server!"))
+        .catch(console.error);
+    client.login
+}
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+import { connectArchi } from "./.github/workflows/client";
 // Target the title and button elements from the HTML
-/*const siteTitle = document.getElementById('title');
+const siteTitle = document.getElementById('title');
 const actionButton = document.getElementById('magicButton');
 
 // Add an event listener to run code whenever the button is clicked
@@ -7,7 +8,7 @@ actionButton.addEventListener('click', () => {
     // Change the text content of the header
     siteTitle.textContent = "The Magic Worked!";
     
+    connectArchi("archipelago.gg:55054","Lucy")
     // Change the color of the text dynamically
     siteTitle.style.color = "#28a745";
 });
-*/
